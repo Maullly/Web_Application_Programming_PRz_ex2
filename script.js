19ce9d5923e8b69aea8bf3f2cfddeee9229e0fac
Maullly <kamalinowski03@gmail.com>
2025-10-21 09:31:28 +0200

    2_1.2

--- script.js
+++ script.js
@@ -34,6 +34,25 @@
       })
   })
 
+  cw1_2.addEventListener("click", function () {
+    answer.textContent = "Loading..."
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+        console.log(posts);
+        let html = '<h2>Lista postów JSONa</h2>';
+        posts.forEach(post => {
+          html += `
+            <div style = "border: 1px groove; background-color: Tomato">
+              <h3>${post.id}. ${post.title}</h3>
+              <p>${post.body}</p>
+            </div>
+          `;
+        });
+        answer.innerHTML = html;
+      })
+  })
+  
   cw2.addEventListener("click", function () {
     //TODO
   })
