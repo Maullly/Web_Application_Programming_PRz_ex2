9069d36b8fd1f478af8fc91425148678021219be
Maullly <kamalinowski03@gmail.com>
2025-10-21 09:24:28 +0200

    2_1.1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (function () {
   const example = document.getElementById('example')
-  const cw1 = document.getElementById('cw1')
+  const cw1 = document.getElementById('1_1')
+  const cw1_2 = document.getElementById('1_2')
+  const cw1_3 = document.getElementById('1_3')
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
@@ -15,7 +17,21 @@
   })
 
   cw1.addEventListener("click", function () {
-    //TODO
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+        console.log(posts);
+        let html = '<h2>Lista postów JSONa</h2>';
+        posts.forEach(post => {
+          html += `
+            <div>
+              <h3>${post.id}. ${post.title}</h3>
+              <p style = "background-color: Tomato;>${post.body}</p>
+            </div>
+          `;
+        });
+        answer.innerHTML = html;
+      })
   })
 
   cw2.addEventListener("click", function () {
